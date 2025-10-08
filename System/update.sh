0e9268c05870faff3e0593b8d9677dc89a447fb3
Arnold Dózsa <33131101+arnolddozsa@users.noreply.github.com>
2025-10-08 14:51:40 +0200

    update sh

--- System/update.sh
+++ System/update.sh
@@ -5,6 +5,7 @@ echo "Használt GIT TOKEN: $GIT_TOKEN" >> update.log
 cd /home/pi/ct-coin-machine
 
 git pull https://$GIT_TOKEN@github.com/mhzq-com/ct-coin-machine.git >> update.log 2>&1
+sudo chmod 755 System/update.sh
 npm install >> update.log 2>&1
 npm run build >> update.log 2>&1
 #pm2 restart your-app-name >> update.log 2>&1  # vagy az alkalmazásod újraindítása
