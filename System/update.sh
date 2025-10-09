48c3f78e32f1fa2969e63e6b98ed8f090bb86b07
Arnold Dózsa <33131101+arnolddozsa@users.noreply.github.com>
2025-10-09 10:37:21 +0200

    Update update.sh

--- System/update.sh
+++ System/update.sh
@@ -9,11 +9,11 @@ echo "Csomagok frissítése ..." #>> update.log
 npm install #>> update.log 2>&1
 echo "Build indítása ..." #>> update.log
 npm run build #>> update.log 2>&1
+echo "Szerver újraindítás előkészítése..."
+# külön folyamatban, kis késleltetéssel indítja újra
+nohup bash -c "sleep 2 && cd /home/pi/ct-coin-machine && nohup npm run start &" >/dev/null 2>&1 &
 echo "Futó folyamatok lezárása ..." #>> update.log
-pkill -f "node"
-echo "Szerver újraindítása ..." #>> update.log
-nohup npm run start &
-#pm2 restart your-app-name >> update.log 2>&1  # vagy az alkalmazásod újraindítása
-
 echo "Frissítés befejezve: $(date)" #>> update.log
 echo "OK"
+pkill -f "node"
+#pm2 restart your-app-name >> update.log 2>&1  # vagy az alkalmazásod újraindítása
